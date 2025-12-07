236c209acf3bb2478e820abecd6bd82899d65cf3
Oracle Jedi <oracle_jedi@yahoo.com>
2025-12-06 16:38:30 -0800

    Create rescan_udev.sh

---
+++ fa_pg_ora_snap/rescan_udev.sh
@@ -0,0 +1,8 @@
+# rescan SCSI bus and check devices are present in /dev/oracleasm
+# use this version when ASM devices are managed by UDEV rules
+# add to sudoers for oracle
+# oracle  ALL=(ALL:ALL) NOPASSWD: /root/udev/rescan_udev.s
+
+/usr/bin/rescan-scsi-bus.sh -r
+
+ls /dev/oracleasm
